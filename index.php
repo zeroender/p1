fa246783c796bf0788b0d8aeeb3efa2c92d287dd
Brian Worthington <worthington.brian@gmail.com>
2017-09-13 12:53:47 -0400

    Pushing inital changes for Project 1

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php include('php/quotegen.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="css/styles.css" />
+  </head>
+
+  <body>
+    <div>
+      <h3>Brian Worthington</h3>
+      <img src="/images/profile.jpg" />
+    </div>
+    <div class="aboutMe">
+      <h3>About Me</h3>
+      <p>Hi there!  I'm a native of Boston, Massachussets.
+      I live on the North shore with my wife Kara and daughter Amelia.
+      I'm currently employed at Oracle, Inc, where I work as a Quality Engineer,
+      testing front end and back end applications.
+      I started learning about web development about 2 years ago,
+      and I'm looking foward to learning a great deal about dynamic web applications in this course!</p>
+    </div>
+    <div class="quote">
+      <h3>Random Quote</h3>
+      <p><?=$quotes[$quote].'<br><br> -'.$quote?></p>
+    </div>
+
+  </body>
+</html>
---
+++ php/quotegen.php
@@ -0,0 +1,14 @@
+<?php
+
+//all quotes from http://www.greeka.com/greece-history/greek-people-quotes.htm
+//I didn't verify them beyond that!
+
+$quotes = [
+  'Socrates' => 'One thing I know, that I know nothing. This is the source of my wisdom.',
+  'Plato' => 'Good people do not need laws to tell them to act responsibly, while bad people will find a way around the laws.',
+  'Aristotle' => 'Anybody can become angry - that is easy, but to be angry with the right person and to the right degree and at the right time and for the right purpose, and in the right way - that is not within everybody\'s power and is not easy',
+  'Epicurus' => 'You don\'t develop courage by being happy in your relationships everyday. You develop it by surviving difficult times and challenging adversity.',
+  'Heraclitus' => 'There is nothing permanent except change'
+];
+
+$quote = array_rand($quotes);
